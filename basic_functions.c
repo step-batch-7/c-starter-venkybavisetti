780d3af0cc44e03a58d9146c4ce2d9c50652fdc7
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-03-29 01:18:59 +0530

    implemented centigrade_to_fahrenheit function

--- basic_functions.c
+++ basic_functions.c
@@ -71,38 +71,38 @@ float centigrade_to_fahrenheit(float centigrade)
 
 int main(void)
 {
-  int even, odd, sqr, cub, gcd1, gcd2, lcm1, lcm2;
-  float principal, interest, period, fahrenheit, centigrade;
+  int num1, num2;
+  float principal, interest, period, temperature;
 
   printf("Finding Even number\n");
   printf("Enter number: ");
-  scanf("%d", &even);
-  printf("%d is an %s number\n\n", even, is_even(even) ? "Even" : "Not Even");
+  scanf("%d", &num1);
+  printf("%d is an %s number\n\n", num1, is_even(num1) ? "Even" : "Not Even");
 
   printf("Finding Odd number\n");
   printf("Enter number: ");
-  scanf("%d", &odd);
-  printf("%d is an %s number\n\n", odd, is_odd(odd) ? "Odd" : "Not Odd");
+  scanf("%d", &num1);
+  printf("%d is an %s number\n\n", num1, is_odd(num1) ? "Odd" : "Not Odd");
 
   printf("Finding Square of a number\n");
   printf("Enter number: ");
-  scanf("%d", &sqr);
-  printf("Square of %d is %ld\n\n", sqr, square(sqr));
+  scanf("%d", &num1);
+  printf("Square of %d is %ld\n\n", num1, square(num1));
 
   printf("Finding Cube of a number\n");
   printf("Enter number: ");
-  scanf("%d", &cub);
-  printf("Cube of %d is %ld\n\n", cub, cube(cub));
+  scanf("%d", &num1);
+  printf("Cube of %d is %ld\n\n", num1, cube(num1));
 
   printf("Finding GCD of Two numbers\n");
   printf("Enter Two numbers(separating by space): ");
-  scanf("%d%d", &gcd1, &gcd2);
-  printf("GCD of %d and %d is %d\n\n", gcd1, gcd2, gcd(gcd1, gcd2));
+  scanf("%d%d", &num1, &num2);
+  printf("GCD of %d and %d is %d\n\n", num1, num2, gcd(num1, num2));
 
   printf("Finding LCM of Two numbers\n");
   printf("Enter Two numbers(separating by space): ");
-  scanf("%d%d", &lcm1, &lcm2);
-  printf("LCM of %d and %d is %ld\n\n", lcm1, lcm2, lcm(lcm1, lcm2));
+  scanf("%d%d", &num1, &num2);
+  printf("LCM of %d and %d is %ld\n\n", num1, num2, lcm(num1, num2));
 
   printf("Finding Simple Interest\n");
   printf("Enter Principal, Interest, Period(separating by space): ");
@@ -116,13 +116,13 @@ int main(void)
 
   printf("convert temperature from fahrenheit to celsius\n");
   printf("Enter temperature in Fahrenheit: ");
-  scanf("%f", &fahrenheit);
-  printf("%.2f Fahrenheit = %.2f Celsius\n\n", fahrenheit, fahrenheit_to_centigrade(fahrenheit));
+  scanf("%f", &temperature);
+  printf("%.2f Fahrenheit = %.2f Celsius\n\n", temperature, fahrenheit_to_centigrade(temperature));
 
   printf("convert temperature from celsius to fahrenheit\n");
   printf("Enter temperature in celsius: ");
-  scanf("%f", &centigrade);
-  printf("%.2f Celsius  = %.2f Fahrenheit\n\n", centigrade, centigrade_to_fahrenheit(centigrade));
+  scanf("%f", &temperature);
+  printf("%.2f Celsius  = %.2f Fahrenheit\n\n", temperature, centigrade_to_fahrenheit(temperature));
 
   return 0;
 }
