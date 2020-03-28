dbc7cb8a88c895bb9e4ec7fddb61c37cce5e8e52
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-03-28 23:06:41 +0530

    implemented is_even function

--- basic_functions.c
+++ basic_functions.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+unsigned char is_even(int);
+
+unsigned char is_even(int num)
+{
+  return num % 2 == 0;
+}
+
 int main(void)
 {
-  printf("hiii");
+  int even;
+
+  printf("Finding Even Number\n");
+  printf("Enter number: ");
+  scanf("%d", &even);
+  printf("%d is an %s number\n", even, is_even(even) ? "Even" : "Not Even");
   return 0;
 }
