fc518d8cc20b55c74564d67ef5c13e090305f7f4
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-03-28 23:30:14 +0530

    implemented square function

--- basic_functions.c
+++ basic_functions.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
 
 unsigned char is_even(int);
+unsigned char is_odd(int);
+long square(int);
 
 unsigned char is_even(int num)
 {
   return num % 2 == 0;
 }
 
+unsigned char is_odd(int num)
+{
+  return !is_even(num);
+}
+
+long square(int num)
+{
+  return num * num;
+}
+
 int main(void)
 {
-  int even, odd;
+  int even, odd, sqr;
 
-  printf("Finding Even Number\n");
+  printf("Finding Even number\n");
   printf("Enter number: ");
   scanf("%d", &even);
   printf("%d is an %s number\n\n", even, is_even(even) ? "Even" : "Not Even");
 
-  printf("Finding Odd Number\n");
+  printf("Finding Odd number\n");
   printf("Enter number: ");
   scanf("%d", &odd);
-  printf("%d is an %s number\n", odd, is_even(odd) ? "Not Odd" : "Odd");
+  printf("%d is an %s number\n\n", odd, is_odd(odd) ? "Odd" : "Not Odd");
+
+  printf("Finding Square of a number\n");
+  printf("Enter number: ");
+  scanf("%d", &sqr);
+  printf("Square of %d is %ld\n\n", sqr, square(sqr));
 
   return 0;
 }
