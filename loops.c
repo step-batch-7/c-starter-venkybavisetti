2fa52bffb4d35e06deab3fe38e8bb025edc3c1a1
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-03-29 14:20:02 +0530

    implemented odd_number_series function

--- loops.c
+++ loops.c
@@ -2,6 +2,7 @@
 
 unsigned long int factorial(int number);
 int fibonacci(int number);
+unsigned int odd_number_series(int number);
 
 unsigned long int factorial(int number)
 {
@@ -16,13 +17,24 @@ unsigned long int factorial(int number)
 int fibonacci(int number)
 {
   unsigned long int previous_num = 0, present_num = 1;
-  printf("%d\n", 0);
-  printf("%d\n", 0);
+  printf("%d\n%d\n", 0, 0);
   for (int index = number; index > 0; index--)
   {
     present_num += previous_num;
     previous_num = present_num - previous_num;
     printf("%lu\n", present_num);
+  }
+  return 0;
+}
+
+unsigned int odd_number_series(int number)
+{
+  for (int index = 0; index <= number; index++)
+  {
+    if (index % 2 == 1)
+    {
+      printf("%d\n", index);
+    }
   }
   return 0;
 }
@@ -40,6 +52,12 @@ int main(void)
   printf("Enter number: ");
   scanf("%d", &num1);
   fibonacci(num1);
+  printf("\n");
+
+  printf("Finding Odd Number Series of an number\n");
+  printf("Enter number: ");
+  scanf("%d", &num1);
+  odd_number_series(num1);
   printf("\n");
 
   return 0;
