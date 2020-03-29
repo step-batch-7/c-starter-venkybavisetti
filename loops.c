0777bdf8351dea4d972db3a685efe549fa77f236
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-03-29 08:54:15 +0530

    implemented fibonacci function

--- loops.c
+++ loops.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 unsigned long int factorial(int number);
+int fibonacci(int number);
 
 unsigned long int factorial(int number)
 {
@@ -12,6 +13,20 @@ unsigned long int factorial(int number)
   return result;
 }
 
+int fibonacci(int number)
+{
+  unsigned long int previous_num = 0, present_num = 1;
+  printf("%d\n", 0);
+  printf("%d\n", 0);
+  for (int index = number; index > 0; index--)
+  {
+    present_num += previous_num;
+    previous_num = present_num - previous_num;
+    printf("%lu\n", present_num);
+  }
+  return 0;
+}
+
 int main(void)
 {
   int num1;
@@ -19,7 +34,13 @@ int main(void)
   printf("Finding Factorial of an number\n");
   printf("Enter number: ");
   scanf("%d", &num1);
-  printf("Factorial of %d is %lu", num1, factorial(num1));
+  printf("Factorial of %d is %lu\n\n", num1, factorial(num1));
+
+  printf("Finding Fibonacci Series of an number\n");
+  printf("Enter number: ");
+  scanf("%d", &num1);
+  fibonacci(num1);
+  printf("\n");
 
   return 0;
 }
