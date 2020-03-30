21a8e1aa5b470087ecbd6edfecfc1c578a9dc0c3
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-03-30 10:16:04 +0530

    implemented currency_breakdown function

--- currency_breakdown.c
+++ currency_breakdown.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <math.h>
+
+unsigned int give_particular_note(unsigned int balance, int note_value)
+{
+  int no_of_notes = floor(balance / note_value);
+  unsigned int remaining_balance = balance - (no_of_notes * note_value);
+  printf("%d x RS %d\n", no_of_notes, note_value);
+  return remaining_balance;
+}
+
+int currency_breakdown(unsigned int amount)
+{
+  unsigned int balance = amount;
+  balance = give_particular_note(balance, 2000.00);
+  balance = give_particular_note(balance, 500);
+  balance = give_particular_note(balance, 200);
+  balance = give_particular_note(balance, 100);
+  balance = give_particular_note(balance, 50);
+  balance = give_particular_note(balance, 10);
+  balance = give_particular_note(balance, 5);
+  balance = give_particular_note(balance, 1);
+  return 0;
+}
 
 int main(void)
 {
-  printf("hi");
+  unsigned int amount;
+  printf("calculating currency breakdown\n");
+  printf("Enter Amount: ");
+  scanf("%u", &amount);
+  currency_breakdown(amount);
 }
