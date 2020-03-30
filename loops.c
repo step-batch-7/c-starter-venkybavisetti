57591ef1906f83a3fa3a5c4e0689cca0baf0126f
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-03-30 07:28:20 +0530

    implemented every_nth_number_bw_numbers function

--- loops.c
+++ loops.c
@@ -104,55 +104,74 @@ int odd_number_series_bw_ranges(int num1, int num2)
   return 0;
 }
 
+int every_nth_number_bw_numbers(unsigned int nth_num, int num1, int num2)
+{
+  int starting_range = fmin(num1, num2);
+  int ending_range = fmax(num1, num2);
+  for (int index = starting_range; index <= ending_range; index += nth_num)
+  {
+    printf("%d", index);
+    ((index + nth_num) > ending_range) ? printf(".") : printf(",");
+  }
+  return 0;
+}
+
 int main(void)
 {
   int num1, num2;
   unsigned int range;
 
-  printf("Finding Factorial of an number\n");
-  printf("Enter number: ");
-  scanf("%d", &num1);
-  printf("Factorial of %d is %lu\n\n", num1, factorial(num1));
+  // printf("Finding Factorial of an number\n");
+  // printf("Enter number: ");
+  // scanf("%d", &num1);
+  // printf("Factorial of %d is %lu\n\n", num1, factorial(num1));
 
-  printf("Finding Fibonacci Series of an number\n");
-  printf("Enter number: ");
-  scanf("%d", &num1);
-  fibonacci(num1);
-  printf("\n");
+  // printf("Finding Fibonacci Series of an number\n");
+  // printf("Enter number: ");
+  // scanf("%d", &num1);
+  // fibonacci(num1);
+  // printf("\n");
 
-  printf("Finding Odd Number Series of an number\n");
-  printf("Enter number: ");
-  scanf("%d", &num1);
-  odd_number_series(num1);
-  printf("\n");
+  // printf("Finding Odd Number Series of an number\n");
+  // printf("Enter number: ");
+  // scanf("%d", &num1);
+  // odd_number_series(num1);
+  // printf("\n");
 
-  printf("Finding Even Number Series of an number\n");
-  printf("Enter number: ");
-  scanf("%d", &num1);
-  even_number_series(num1);
-  printf("\n");
+  // printf("Finding Even Number Series of an number\n");
+  // printf("Enter number: ");
+  // scanf("%d", &num1);
+  // even_number_series(num1);
+  // printf("\n");
 
-  printf("Finding Multiplication Table\n");
-  printf("Enter Multiplier number and range(separating by space): ");
-  scanf("%d%u", &num1, &range);
-  multiplication_table(num1, range);
-  printf("\n");
+  // printf("Finding Multiplication Table\n");
+  // printf("Enter Multiplier number and range(separating by space): ");
+  // scanf("%d%u", &num1, &range);
+  // multiplication_table(num1, range);
+  // printf("\n");
 
-  printf("Finding Sum of any N numbers\n");
-  printf("Enter the starting and ending range for sum of numbers(separating by space): ");
-  scanf("%d%d", &num1, &num2);
-  printf("The Sum of numbers between %d and %d is: %ld\n\n", num1, num2, sum_of_any_n_numbers(num1, num2));
+  // printf("Finding Sum of any N numbers\n");
+  // printf("Enter the starting and ending range(separating by space): ");
+  // scanf("%d%d", &num1, &num2);
+  // printf("The Sum of numbers between %d and %d is: %ld\n\n", num1, num2, sum_of_any_n_numbers(num1, num2));
 
-  printf("Finding Product of any N numbers\n");
-  printf("Enter the starting and ending range for Product of numbers(separating by space): ");
-  scanf("%d%d", &num1, &num2);
-  printf("The Product of numbers between %d and %d is: %ld\n\n", num1, num2, product_of_any_n_numbers(num1, num2));
+  // printf("Finding Product of any N numbers\n");
+  // printf("Enter the starting and ending range(separating by space): ");
+  // scanf("%d%d", &num1, &num2);
+  // printf("The Product of numbers between %d and %d is: %ld\n\n", num1, num2, product_of_any_n_numbers(num1, num2));
 
-  printf("Printing all odd numbers between any two numbers\n");
-  printf("Enter the starting and ending range for odd numbers(separating by space): ");
-  scanf("%d%d", &num1, &num2);
-  printf("The odd numbers between %d and %d is: \n", num1, num2);
-  odd_number_series_bw_ranges(num1, num2);
+  // printf("Printing all odd numbers between any two numbers\n");
+  // printf("Enter the starting and ending range(separating by space): ");
+  // scanf("%d%d", &num1, &num2);
+  // printf("The odd numbers between %d and %d is: \n", num1, num2);
+  // odd_number_series_bw_ranges(num1, num2);
+  // printf("\n");
+
+  printf("Printing every nth number between any two number \n");
+  printf("Enter the increment number, starting and ending range(separating by space): ");
+  scanf("%u%d%d", &range, &num1, &num2);
+  printf("The every %uth number between %d and %d is: \n", range, num1, num2);
+  every_nth_number_bw_numbers(range, num1, num2);
   printf("\n");
 
   return 0;
