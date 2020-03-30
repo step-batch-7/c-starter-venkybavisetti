f48cf4321d9a2acc644babe5560f01c91593dbec
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-03-30 10:25:57 +0530

    implemented currency_breakdown function

--- basic_functions.c
+++ basic_functions.c
@@ -78,7 +78,7 @@ int greatest_of_three(int num1, int num2, int num3)
 
 float average_of_three(int num1, int num2, int num3)
 {
-  return (num1 + num2 + num3) / 3;
+  return (num1 + num2 + num3) / (float)3;
 }
 
 int main(void)
